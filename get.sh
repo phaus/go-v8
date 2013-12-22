42ad7ddea99cbd8aea3ec46c94a3ca7b168a95b1
达达 <bg5sbk@gmail.com>
2013-12-23 00:18:15 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!sh
 
 if [ $# -eq 0 ]; then
 	echo >&2 "useage: ./get.sh the_install_dir"
@@ -12,11 +12,11 @@ mkdir $install_dir
 cd $install_dir
 export GOPATH=`pwd`
 
-mkdir -p bin pkg src/github.com/idada/go.v8
-cd src/github.com/idada/go.v8
+mkdir -p bin pkg src/github.com/idada/v8.go
+cd src/github.com/idada/v8.go
 
 git init
-git remote add origin https://github.com/idada/go.v8
+git remote add origin https://github.com/idada/v8.go
 git pull origin master
 
 ./install.sh
