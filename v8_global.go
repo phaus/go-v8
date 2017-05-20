333d4e40ead4c7ec9f1a567b492da3e37d27ea2e
saibing <saibingdeng@gmail.com>
2017-05-20 22:16:22 +0800

    add typescript compile script test

--- v8_global.go
+++ v8_global.go
@@ -10,31 +10,34 @@ type embedable struct {
 	data interface{}
 }
 
-func (this embedable) GetPrivateData() interface{} {
-	return this.data
+//GetPrivateData get private data
+func (e embedable) GetPrivateData() interface{} {
+	return e.data
 }
 
-func (this *embedable) SetPrivateData(data interface{}) {
-	this.data = data
+//SetPrivateData set private data
+func (e *embedable) SetPrivateData(data interface{}) {
+	e.data = data
 }
 
+//GetVersion get v8 version
 func GetVersion() string {
 	return C.GoString(C.V8_GetVersion())
 }
 
-
-//number of isolate data slots
-func GetIsolateNumberOfDataSlots() uint{
+//GetIsolateNumberOfDataSlots get number of isolate data slots
+func GetIsolateNumberOfDataSlots() uint {
 	return uint(C.V8_Isolate_GetNumberOfDataSlots())
 }
 
+//SetFlagsFromString set flags from string.
 func SetFlagsFromString(cmd string) {
 	cs := C.CString(cmd)
 	defer C.free(unsafe.Pointer(cs))
 	C.V8_SetFlagsFromString(cs, C.int(len(cmd)))
 }
 
-// Set default array buffer allocator to V8 for
+//UseDefaultArrayBufferAllocator Set default array buffer allocator to V8 for
 // ArrayBuffer, ArrayBufferView, Int8Array...
 // If you want to use your own allocator. You can implement it in C++
 // and invoke v8::SetArrayBufferAllocator by your self
